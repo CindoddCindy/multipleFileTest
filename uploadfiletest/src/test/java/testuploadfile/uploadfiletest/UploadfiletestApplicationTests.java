2bd76af6aa050d174cb655e8dc01714e0e2d4a5b
CindoddCindy <cindodcindy@gmail.com>
2021-05-13 19:18:17 +0700

    tambah kelas model

---
+++ uploadfiletest/src/test/java/testuploadfile/uploadfiletest/UploadfiletestApplicationTests.java
@@ -0,0 +1,13 @@
+package testuploadfile.uploadfiletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadfiletestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
